632edece5559dd1dffb2ded5621cc1fd813ca6b0
Rudnyk Bohdan <rudnikboda@gmail.com>
2024-03-19 18:40:55 +0200

    Added blank functions to input and output

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+
+
+def input_text_from_console():
+    pass
+
+def read_from_file_using_py(filename):
+    pass
+
+def read_from_file_using_pandas(filename):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,7 @@
+
+
+def output_text_to_console(text):
+    pass
+
+def write_to_file_using_py(filename, data):
+    pass
